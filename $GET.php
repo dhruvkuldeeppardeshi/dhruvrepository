c5d40bb6f2f9f019ccf0c03240830db2fc8e2e88
dhruvkuldeeppardeshi <dhruv.pardeshi87@gmail.com>
2025-08-02 11:46:26 +0530

    Add files via upload

---
+++ $GET.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>FOR DEMO</title>
+</head>
+<body>
+<h1>FOR DEMO</h1>
+<?php
+echo"welcome";
+echo $get ["name"]"<br>";
+
+echo"Your email address is:";
+echo $get ["email"];
+?>
+</body>
+</html>
+
